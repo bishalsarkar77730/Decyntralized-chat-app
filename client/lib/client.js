3c15c48e39171c1297dc5c75237a1f14ca89f95b
Bishal Sarkar <69084883+bishalsarkar77730@users.noreply.github.com>
2022-11-05 21:07:04 +0530

    solving errors

---
+++ client/lib/client.js
@@ -0,0 +1,9 @@
+import sanityClient from '@sanity/client'
+export const clieSANITY_PROJECT_IDnt = sanityClient({
+  projectId: "lc7efl95",
+  dataset: "production",
+  apiVersion: "v1",
+  token:
+    "skyFedqfnxTE04cUImr43A2YiuU7UnrktetWq6LznaVgrE3CrxNdn1KDD1yvyrK9FlR1EwXWDkHswUcuJXbCEZpC0hHSCTIzoOtE6ZfX1u1xCum8CWZQHiDmfqyeqpS9NQqQKU4aFVIx188xr5hnaM17kTTCtxVfMZodjbzuzJX4pw78fyTh",
+  useCdn: false,
+});
